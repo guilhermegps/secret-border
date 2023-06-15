cf61243f407dd6075f564ffe530eef38ff027630
Guilherme Gps <guilherme.ps20@gmail.com>
2023-06-15 07:41:06 -0300

    Methods to create a file encrypted and to read the encrypted file

--- src/main/java/com/secretborder/crypto/Crypto.java
+++ src/main/java/com/secretborder/crypto/Crypto.java
@@ -13,7 +13,6 @@ import java.security.interfaces.ECPrivateKey;
 import java.security.spec.ECGenParameterSpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -26,6 +25,7 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.Arrays;
 
 import com.secretborder.util.KeyUtil;
 
@@ -70,19 +70,33 @@ public class Crypto {
         return Point.bytesFromPoint(ret);
     }
 
-//	public static String encrypt(byte[] input, String password)
-//			throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
-//			InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-//		
-//	}
-//
-//	public static byte[] decrypt(byte[] cipherBytes, String password)
-//			throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
-//			InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-//		
-//	}
+	public static byte[] encrypt(byte[] input, String password) {
+		try {
+			var iv = generateIv();
+			var salt = generateSalt();
+			
+			var key = getKeyFromPassword(password, salt);
+			byte[] outputBytes = encrypt(input, key, iv);
+			return Arrays.concatenate(salt, iv.getIV(), outputBytes);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-	public static byte[] encrypt(byte[] input, SecretKey key, IvParameterSpec iv)
+	public static byte[] decrypt(byte[] cipherBytes, String password) {
+		try {
+			var salt = Arrays.copyOfRange(cipherBytes, 0, 8);
+			var iv = Arrays.copyOfRange(cipherBytes, 9, 24);
+			
+			var key = getKeyFromPassword(password, salt);
+			byte[] outputBytes = decrypt(Arrays.copyOfRange(cipherBytes, 25, cipherBytes.length), key, new IvParameterSpec(iv));
+			return outputBytes;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+    private static byte[] encrypt(byte[] input, SecretKey key, IvParameterSpec iv)
 			throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
 			InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
 
@@ -91,7 +105,7 @@ public class Crypto {
 		return cipher.doFinal(input);
 	}
 
-	public static byte[] decrypt(byte[] cipherBytes, SecretKey key, IvParameterSpec iv)
+	private static byte[] decrypt(byte[] cipherBytes, SecretKey key, IvParameterSpec iv)
 			throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
 			InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
 
@@ -100,19 +114,26 @@ public class Crypto {
 		return cipher.doFinal(cipherBytes);
 	}
 
-	public static SecretKey getKeyFromPassword(String password, String salt)
+	private static SecretKey getKeyFromPassword(String password, byte[] salt)
 			throws NoSuchAlgorithmException, InvalidKeySpecException {
 
 		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
 		SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
 		return secret;
 	}
 
-	public static IvParameterSpec generateIv() {
+	private static IvParameterSpec generateIv() {
 		byte[] iv = new byte[16];
 		new SecureRandom().nextBytes(iv);
 		return new IvParameterSpec(iv);
 	}
+
+    private static byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[8];
+        random.nextBytes(salt);
+        return salt;
+    }
 
 }
